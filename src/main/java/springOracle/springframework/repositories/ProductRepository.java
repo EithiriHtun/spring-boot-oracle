d2b63958d8b0d92fba6dacf9dcf1579748e602c9
eithiri <eithiritun1997@gmail.com>
2020-12-18 18:00:27 +0630

    commit : 514. Spring Boot Oracle Database Example

--- src/main/java/springOracle/springframework/repositories/ProductRepository.java
+++ src/main/java/springOracle/springframework/repositories/ProductRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.repository.CrudRepository;
  * Created by jt on 1/10/17.
  */
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    Product findOne(Long id);
+
+    void delete(Long id);
 }
